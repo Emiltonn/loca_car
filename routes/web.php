60b375ca8a6e82eebb05ef0ab02f89b5078c7a70
EMiltonn <emanuelmilton@outlook.com>
2020-12-15 18:22:19 -0400

    Adicionado Controllers, Models e Rotas

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AccountControler;
+use App\Http\Controllers\TransactionControler;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('account', AccountController::class);
+Route::resource('transaction', TransactionController::class);
